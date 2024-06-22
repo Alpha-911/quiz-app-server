9d978e1a54b9f3218e338660eec7d0aad6904b3c
Rishu Raj <92977357+Alpha-911@users.noreply.github.com>
2024-06-22 15:53:38 +0530

    Update app.js
    added CORS headers

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const feedbackRoute = require('./routes/feedbackRoute')
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 app.use('/', feedbackRoute)
 
 module.exports = app;
